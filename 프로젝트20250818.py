26d53a8eba631491ade522eddc8bb2851e64b3b4
kmh1324 <gimm85464@gmail.com>
2025-08-19 19:01:54 +0900

    Add files via upload

---
+++ 프로젝트20250818.py
@@ -0,0 +1,12 @@
+##num1, num2, num3 = input("전화번호 입력 : ").split("-")
+##print(num1)
+##print(num2)
+##print(num3)
+
+
+##file_name = input("파일명을 입력해주세요 : ")
+##print("%s 파일을 %s 파일로 변경하였습니다." % (file_name, file_name.replace("jpg", "png")))
+
+
+print("Hello, Python! Hello String!".upper())
+print("Hello, Python! Hello String!".lower())
