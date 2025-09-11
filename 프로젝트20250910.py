bc281b3dd7ac801e670f693a7903760a3ad5f196
kmh1324 <gimm85464@gmail.com>
2025-09-11 19:16:32 +0900

    Add files via upload

---
+++ 프로젝트20250910.py
@@ -0,0 +1,21 @@
+##import datetime
+##input = input("입력: ")
+##
+##if "안녕" in input:
+##    print(f"> 안녕하세요")
+##elif "몇 시" in input:
+##    time = datetime.datetime.now()
+##    print(f"> 지금은 {time.hour}시입니다.")
+##else :
+##    print(f"> input")
+
+
+##num = int(input("정수를 입력해주세요: "))
+##
+##for i in range(2, 6):
+##    if num % i == 0:
+##        print(f"{num}은 {i}로 나누어 떨어지는 숫자입니다.")
+##    else :
+##        print(f"{num}은 {i}로 나누어 떨어지는 숫자가 아닙니다.")
+
+
