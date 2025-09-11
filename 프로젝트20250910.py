c77b347c9c86c8acc8cacd5e24bb9041a3710c1d
kmh1324 <gimm85464@gmail.com>
2025-09-11 20:06:47 +0900

    Update 프로젝트20250910.py

--- 프로젝트20250910.py
+++ 프로젝트20250910.py
@@ -7,7 +7,7 @@
 ##    time = datetime.datetime.now()
 ##    print(f"> 지금은 {time.hour}시입니다.")
 ##else :
-##    print(f"> input")
+##    print(f"> {input}")
 
 
 ##num = int(input("정수를 입력해주세요: "))
@@ -17,5 +17,6 @@
 ##        print(f"{num}은 {i}로 나누어 떨어지는 숫자입니다.")
 ##    else :
 ##        print(f"{num}은 {i}로 나누어 떨어지는 숫자가 아닙니다.")
+
 
 
